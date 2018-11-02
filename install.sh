9d01b3b13b1c31cc7aa21a680e14b70d9e488f80
David Lim <nicholai.david@gmail.com>
2018-11-02 10:05:32 +0800

    Add dynamic paths to install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-ln -sf ~/dev/my-environment/vimrc ~/.vimrc
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+ln -sf $DIR/vimrc ~/.vimrc
+ln -sf $DIR/bash_profile ~/.bash_profile
