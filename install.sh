47bd6320e30383b5799c73653d86ea4c868be17d
David Lim <nicholai.david@gmail.com>
2018-11-02 12:20:02 +0800

    Include slatejs in install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 ln -sf $DIR/vimrc ~/.vimrc
 ln -sf $DIR/bash_profile ~/.bash_profile
 ln -sf $DIR/tmux.conf ~/.tmux.conf
+ln -sf $DIR/slate.js ~/.slate.js
